5c8229c0b8cf8c302581d268d2feb960d63803eb
Sergey Skaridov <s.skaridov@gmail.com>
2021-12-15 11:02:40 +0300

    use bulk result autocreating

--- src/Reporter.php
+++ src/Reporter.php
@@ -10,7 +10,6 @@ use PHPUnit\Runner\AfterSuccessfulTestHook;
 use PHPUnit\Runner\AfterTestErrorHook;
 use PHPUnit\Runner\AfterTestFailureHook;
 use PHPUnit\Runner\BeforeFirstTestHook;
-use PHPUnit\Util\Exception;
 use Qase\Client\ApiException;
 
 class Reporter implements AfterSuccessfulTestHook, AfterSkippedTestHook, AfterTestFailureHook, AfterTestErrorHook, AfterLastTestHook, BeforeFirstTestHook
@@ -23,30 +22,11 @@ class Reporter implements AfterSuccessfulTestHook, AfterSkippedTestHook, AfterTe
     private const SKIPPED = 'skipped';
     private const FAILED = 'failed';
 
-    /**
-     * @var RunResult
-     */
-    private $runResult;
-
-    /**
-     * @var Repository
-     */
-    private $repo;
-
-    /**
-     * @var ResultHandler
-     */
-    private $resultHandler;
-
-    /**
-     * @var ConsoleLogger
-     */
-    private $logger;
-
-    /**
-     * @var Config
-     */
-    private $config;
+    private RunResult $runResult;
+    private Repository $repo;
+    private ResultHandler $resultHandler;
+    private ConsoleLogger $logger;
+    private Config $config;
 
     public function __construct()
     {
@@ -61,7 +41,7 @@ class Reporter implements AfterSuccessfulTestHook, AfterSkippedTestHook, AfterTe
             $this->config->getCompleteRunAfterSubmit(),
         );
 
-        $this->repo = new Repository($this->runResult->getProjectCode(), $this->logger);
+        $this->repo = new Repository($this->runResult->getProjectCode());
         $this->resultHandler = new ResultHandler($this->repo, $this->logger);
     }
 
@@ -110,77 +90,21 @@ class Reporter implements AfterSuccessfulTestHook, AfterSkippedTestHook, AfterTe
 
     private function accumulateTestResult(string $status, string $test, float $time, string $message = null): void
     {
-        $caseId = $this->getCaseId($test);
-
-        if (!$caseId) {
-            return;
-        }
-
-        if (preg_match('/with data set "(.+)"|(#\d+)/U', $test, $matches, PREG_UNMATCHED_AS_NULL) === 1) {
-            $params = $matches[1] ?? $matches[2];
-        }
-
-        $this->runResult->addResult(
-            $caseId,
-            [
-                'status' => $status,
-                'time' => $time,
-                'testName' => $test,
-                'message' => $message,
-                'params' => $params ?? null,
-            ]
-        );
+        $this->runResult->addResult([
+            'status' => $status,
+            'time' => $time,
+            'full_test_name' => $test,
+            'stacktrace' => $status === self::FAILED ? $message : null,
+        ]);
     }
 
-    private function getCaseId(string $testName): ?int
-    {
-        if (!preg_match_all('/(?P<namespace>.+)::(?P<methodName>\w+)/', $testName, $testNameMatches)) {
-            $this->logger->writeln("WARNING: Could not parse test name '{$testName}'");
-            return null;
-        }
-
-        $namespace = $testNameMatches['namespace'][0];
-        $methodName = $testNameMatches['methodName'][0];
-
-        $caseTitle = $this->clearPrefix($methodName, ['test']);
-        $suiteTitle = self::ROOT_SUITE_TITLE . '\\' . $this->clearPrefix($namespace, ['Test\\', 'Tests\\']);
-
-        try {
-            $reflection = new \ReflectionMethod($namespace, $methodName);
-        } catch (\ReflectionException $e) {
-            throw new Exception($e->getMessage(), (int)$e->getCode(), $e);
-        }
-
-        $docComment = $reflection->getDocComment();
-        if (!$docComment || !preg_match_all('/\@qaseId?[ \t]+(?P<caseId>.*?)[ \t]*\r?$/m', $docComment, $qaseIdMatches)) {
-            return $this->repo->findOrCreateCase($suiteTitle, $caseTitle);
-        }
-
-        if (!($qaseIdMatches['caseId'][0] ?? false)) {
-            return $this->repo->findOrCreateCase($suiteTitle, $caseTitle);
-        }
-
-        $caseId = (int)$qaseIdMatches['caseId'][0];
-
-        if (!$this->repo->caseIdExists($caseId)) {
-            $this->logger->writeln(sprintf(
-                "%s::%s skipped due to @qaseId contains unknown Qase ID: %s",
-                $namespace,
-                $methodName,
-                $caseId
-            ));
-
-            return null;
-        }
-
-        return $caseId;
-    }
 
     public function executeAfterLastTest(): void
     {
         try {
             $this->resultHandler->handle(
-                $this->runResult
+                $this->runResult,
+                $this->config->getRootSuiteTitle() ?: self::ROOT_SUITE_TITLE,
             );
         } catch (\Exception $e) {
             $this->logger->writeln('An exception occurred');
@@ -203,22 +127,5 @@ class Reporter implements AfterSuccessfulTestHook, AfterSkippedTestHook, AfterTe
 
             throw $e;
         }
-    }
-
-    /**
-     * @param string $title
-     * @param array[string] $prefixes
-     * @return string
-     */
-    private function clearPrefix(string $title, array $prefixes): string
-    {
-        foreach ($prefixes as $prefix) {
-            $prefixLength = mb_strlen($prefix);
-            if (strncmp($title, $prefix, $prefixLength) === 0) {
-                return mb_substr($title, $prefixLength);
-            }
-        }
-
-        return $title;
     }
 }
--- src/Repository.php
+++ src/Repository.php
@@ -9,72 +9,22 @@ use Qase\Client\Api\CasesApi;
 use Qase\Client\Api\ProjectsApi;
 use Qase\Client\Api\ResultsApi;
 use Qase\Client\Api\RunsApi;
-use Qase\Client\Api\SuitesApi;
 use Qase\Client\Configuration;
-use Qase\Client\Model\SuiteCreate;
-use Qase\Client\Model\TestCaseCreate;
 
 class Repository
 {
-    /**
-     * @var string
-     */
-    private $projectCode;
+    private string $projectCode;
+    private RunsApi $runsApi;
+    private ProjectsApi $projectsApi;
+    private ResultsApi $resultsApi;
+    private CasesApi $casesApi;
+    private array $existingCases = [];
+    private array $existingCasesGroupedBySuite = [];
+    private \GuzzleHttp\Client $client;
 
-    /**
-     * @var RunsApi
-     */
-    private $runsApi;
-
-    /**
-     * @var ProjectsApi
-     */
-    private $projectsApi;
-
-    /**
-     * @var ResultsApi
-     */
-    private $resultsApi;
-
-    /**
-     * @var CasesApi
-     */
-    private $casesApi;
-
-    /**
-     * @var SuitesApi
-     */
-    private $suitesApi;
-
-    /**
-     * @var array
-     */
-    private $suitesFlat;
-
-    /**
-     * @var ConsoleLogger
-     */
-    private $logger;
-
-    /**
-     * @var array
-     */
-    private $existingCases = [];
-
-    /**
-     * @var array
-     */
-    private $existingCasesGroupedBySuite = [];
-
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    private $client;
-
-    public function __construct(string $projectCode, ConsoleLogger $logger)
+    public function __construct(string $projectCode)
     {
         $this->projectCode = $projectCode;
-        $this->logger = $logger;
     }
 
     /**
@@ -115,13 +65,11 @@ class Repository
         $this->projectsApi = new ProjectsApi($this->client, $config);
         $this->resultsApi = new ResultsApi($this->client, $config);
         $this->casesApi = new CasesApi($this->client, $config);
-        $this->suitesApi = new SuitesApi($this->client, $config);
 
         $this->getCases();
-        $this->getSuites();
     }
 
-    public function getCases(): void
+    private function getCases(): void
     {
         $cases = $this->getAllEntities($this->casesApi->getCasesRequest($this->projectCode));
 
@@ -129,25 +77,6 @@ class Repository
             $this->existingCases[$case['id']] = $case['title'];
             $this->existingCasesGroupedBySuite[$case['suite_id'] ?: 0][$case['title']] = $case['id'];
         }
-    }
-
-    public function getSuites(): void
-    {
-        $suites = $this->getAllEntities($this->suitesApi->getSuitesRequest($this->projectCode));
-
-        $suites = array_map(
-            static function (array $suite) {
-                return [
-                    'id' => $suite['id'],
-                    'parent_id' => $suite['parent_id'],
-                    'title' => $suite['title'],
-                ];
-            },
-            $suites
-        );
-
-        $suitesTree = $this->buildSuitesTree($suites);
-        $this->suitesFlat = $this->flatArray($suitesTree);
     }
 
     private function getAllEntities(Request $request): array
@@ -190,139 +119,8 @@ class Repository
         return $response['result'];
     }
 
-    public function findOrCreateCase(string $suiteTitle, string $testTitle): int
-    {
-        $caseId = $this->findCaseByTitle($suiteTitle, $testTitle);
-
-        if ($caseId) {
-            return $caseId;
-        }
-
-        return $this->createCase($suiteTitle, $testTitle);
-    }
-
-    public function findCaseByTitle(string $suiteTitle, string $testTitle): ?int
-    {
-        if (!isset($this->suitesFlat[$suiteTitle], $this->existingCasesGroupedBySuite[$this->suitesFlat[$suiteTitle]][$testTitle])) {
-            return null;
-        }
-
-        return $this->existingCasesGroupedBySuite[$this->suitesFlat[$suiteTitle]][$testTitle];
-    }
-
-    private function createCase(string $suiteTitle, string $testTitle): int
-    {
-        $suiteId = $this->findOrCreateSuite($suiteTitle);
-
-        $case = $this->casesApi->createCase(
-            $this->projectCode,
-            new TestCaseCreate([
-                'title' => $testTitle,
-                'automation' => 2,
-                'suiteId' => $suiteId,
-            ])
-        );
-
-        $this->existingCasesGroupedBySuite[$suiteId][$testTitle] = $case->getResult()->getId();
-
-        $this->logger->writeln("created new test case '{$testTitle}' with ID " . $case->getResult()->getId());
-
-        return $case->getResult()->getId();
-    }
-
-    private function findOrCreateSuite(string $suiteTitle): int
-    {
-        if (isset($this->suitesFlat[$suiteTitle])) {
-            return $this->suitesFlat[$suiteTitle];
-        }
-
-        $suitesToCreate = [];
-        $partialSuitTitle = $suiteTitle;
-        $suitePartsCount = substr_count($suiteTitle, '\\') + 1;
-
-        for ($i = 1; $i <= $suitePartsCount; $i++) {
-            if (isset($this->suitesFlat[$partialSuitTitle])) {
-                break;
-            }
-
-            $dividerPosition = mb_strrpos($partialSuitTitle, '\\');
-
-            if ($dividerPosition === false) {
-                $suitesToCreate[] = [
-                    'short' => $partialSuitTitle,
-                    'full' => $partialSuitTitle,
-                ];
-                break;
-            }
-
-            $suitesToCreate[] = [
-                'short' => ltrim(mb_substr($partialSuitTitle, $dividerPosition), '\\'),
-                'full' => $partialSuitTitle,
-            ];
-
-            $partialSuitTitle = mb_substr($partialSuitTitle, 0, $dividerPosition);
-        }
-
-        $parentId = $this->suitesFlat[$partialSuitTitle] ?? null;
-        foreach (array_reverse($suitesToCreate) as $suiteName) {
-            $newSuite = $this->suitesApi->createSuite(
-                $this->projectCode,
-                new SuiteCreate([
-                    'title' => $suiteName['short'],
-                    'parentId' => $parentId,
-                ])
-            );
-
-            $this->suitesFlat[$suiteName['full']] = $newSuite->getResult()->getId();
-            $parentId = $newSuite->getResult()->getId();
-
-            $this->logger->writeln("created new suite '{$suiteName['full']}' with ID " . $newSuite->getResult()->getId());
-        }
-
-        if (!isset($newSuite)) {
-            throw new \RuntimeException("could not create a suite '{$suiteTitle}'");
-        }
-
-        return $newSuite->getResult()->getId();
-    }
-
     public function caseIdExists(int $caseId): bool
     {
         return isset($this->existingCases[$caseId]);
-    }
-
-    public function flatArray(array $arr, string $prefix = ''): array
-    {
-        $flatten = [];
-
-        foreach ($arr as $k => $v) {
-            if (is_array($v) && array_keys($v) !== range(0, count($v) - 1)) {
-                $flatten = array_merge($flatten, $this->flatArray($v, $prefix . (isset($v['title']) ? '\\' . $v['title'] : '')));
-            } else {
-                if ($k === 'id') {
-                    $flatten[ltrim($prefix, '\\')] = $v;
-                }
-            }
-        }
-
-        return $flatten;
-    }
-
-    public function buildSuitesTree(array &$elements, ?int $parentId = null, $rootField = 'id'): array
-    {
-        $branch = [];
-
-        foreach ($elements as &$element) {
-            if ($element['parent_id'] === $parentId) {
-                $children = self::buildSuitesTree($elements, $element['id'], $rootField);
-                if ($children) {
-                    $element['suites'] = $children;
-                }
-                $branch[$element[$rootField]] = $element;
-                unset($element);
-            }
-        }
-
-        return $branch;
     }
 }
