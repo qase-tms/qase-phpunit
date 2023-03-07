2869e0795d1ed8e80a4cbf8b66001b0098ff0573
Sergey Skaridov <s.skaridov@gmail.com>
2023-03-07 21:46:43 +0300

    fix composer version check

--- src/HeaderManager.php
+++ src/HeaderManager.php
@@ -15,7 +15,7 @@ class HeaderManager extends \Qase\PhpCommons\Utils\HeaderManager
         $reporterVersion = $this->composerPackages['qase/phpunit-reporter'] ?? self::UNDEFINED_HEADER;
 
         if (is_callable('shell_exec') && false === stripos(ini_get('disable_functions'), 'shell_exec')) {
-            $composerOutput = shell_exec('composer -V');
+            $composerOutput = shell_exec('composer -V 2>/dev/null') ?? '';
             preg_match('/Composer version\s(?P<version>(.+))\s/U', $composerOutput, $composerMatches);
         }
         $composerVersion = $composerMatches['version'] ?? self::UNDEFINED_HEADER;
